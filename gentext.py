7af6d8b735c44747964fc5e07dad97f23f359317
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2017-03-13 20:48:27 -0500

    restructure

--- gentext.py
+++ gentext.py
@@ -1,23 +1,6 @@
 import random, itertools, loadlang
-def evalmorph(cond, morph):
-    if cond:
-        for c in cond.split():
-            if morph.props[c.split('=')[0]] != c.split('=')[1]:
-                return False
-        return True
-    else:
-        return morph != None
 def pickword(lang, ask):
-    if ask.endswith(')'):
-        pos, conds = ask[:-1].split('(', 1)
-    else:
-        pos = ask
-        conds = ""
-    words = []
-    for w in loadlang.Morpheme.morphemelist[lang][pos].values():
-        if evalmorph(conds, w):
-            words.append(w)
-    return random.choice(words)
+    return random.choice(list(loadlang.Morpheme.findpos(lang, ask)))
 def gennode(outline, name, depth):
     if name:
         n, a = outline[name]
@@ -30,22 +13,18 @@ def gennode(outline, name, depth):
     else:
         todo = outline[outline.start][0]
         depth = 1
-    def dostring(s, lang, depth, outline): #returns value, varname
-        if s[0] == '$':
-            if ':' in s:
-                v, r = dostring(s.split(':', 1)[1], lang, depth, outline)
-                return v, s.split(':')[0]
-            else:
-                return -1, s
-        elif s[-1] in '*?':
-            if random.randint(0,100) < 10/depth:
-                return dostring(s[:-1], lang, depth, outline)[0], None
-            else:
-                return None, None
-        elif s[0] == '#':
-            return pickword(lang, s[1:]), None
+    def dostring(var, lang, depth, outline): #returns value, varname
+        if not var.nodetype:
+            return -1, var.name or None
+        s = var.nodetype
+        if s[-1] in '*?':
+            s = s[:-1]
+            if not (random.randint(0,100) < 10/depth):
+                return None, var.name or None
+        if s[0] == '#':
+            return pickword(lang, var), var.name
         else:
-            return gennode(outline, s, depth+1), None
+            return gennode(outline, s, depth+1), var.name or None
     def iterchildren(intodo, depth, outline):
         if isinstance(intodo, loadlang.SyntaxNode):
             todo = intodo
@@ -56,22 +35,14 @@ def gennode(outline, name, depth):
                 l, r = dostring(v, intodo.lang, depth, outline)
                 names[r] = l
             for i, conds in enumerate(intodo.conds):
-                ok = True
-                for c in conds:
-                    if '(' in c:
-                        ok = evalmorph(c[:-1].split('(')[1], names[c.split('(')[0]])
-                    else:
-                        ok = evalmorph('', names[c])
-                    if not ok:
-                        break
-                if ok:
+                if all([c.findandcheck(names) for c in conds]):
                     todo = intodo.ops[i]
                     break
         ret = []
         for ch in todo.children:
             if ch == None:
                 ret.append(None)
-            elif isinstance(ch, str):
+            elif isinstance(ch, loadlang.Variable):
                 val, var = dostring(ch, todo.lang, depth, outline)
                 if var and val == -1:
                     ret.append(names[var])
@@ -81,30 +52,6 @@ def gennode(outline, name, depth):
                 ret.append(iterchildren(ch, depth, outline))
         return todo.swapchildren(ret)
     return iterchildren(todo, depth, outline)
-def getnames(node, form, callback):
-    names = {}
-    for i, ch in enumerate(form.children):
-        if isinstance(ch, loadlang.SyntaxNode):
-            for k, v in getnames(node[i], ch, callback).items():
-                names[k] = v
-                assert(isinstance(v, list))
-        elif ch == None:
-            pass
-        else:
-            names[ch.split(':')[0]] = callback(node[i])
-            #TODO: This could result in translating the tree many times
-            #maybe try caching it somehow?
-    return names
-def putnames(names, form):
-    if form == None:
-        return [None]
-    elif isinstance(form, loadlang.SyntaxNode):
-        ls = [putnames(names, f) for f in form.children]
-        return [form.swapchildren(chs) for chs in itertools.product(*ls)]
-    elif isinstance(form, loadlang.Morpheme):
-        return [form]
-    else:
-        return names[form] if isinstance(names[form], list) else [names[form]]
 def translatetree(tree, tolang): #general node patterns
     if isinstance(tree, loadlang.Morpheme):
         ret = []
@@ -122,8 +69,7 @@ def translatetree(tree, tolang): #general node patterns
     else:
         ret = []
         for tr in tree.gettranslations(tolang):
-            assert(tree.nodetype == tr.form.nodetype)
-            ret += putnames(getnames(tree, tr.form, lambda node: translatetree(node, tolang)), tr.result)
+            ret += tree.applytranslation(tr, lambda x: translatetree(x, tolang))
         return ret
 def lexicaltransform(tree, tolang): #individual words like tan
     def finddepth(pat):
@@ -144,14 +90,10 @@ def lexicaltransform(tree, tolang): #individual words like tan
             nodes.append(t if isinstance(t, list) else [t])
             pats += p
         trs = [tree.swapchildren(x) for x in itertools.product(*nodes)]
-        rettr = []
         retpats = [[x[0]-1, x[1]] for x in pats if x[0] > 0]
         for p in pats:
             if p[0] == 0:
-                tls = []
-                for tr in trs:
-                    tls += putnames(getnames(tr, p[1].form, lambda x: x), p[1].result)
-                trs = tls
+                trs = list(itertools.chain(*[tr.applytranslation(p[1]) for tr in trs]))
         return trs, retpats
     elif isinstance(tree, loadlang.Morpheme):
         if tree.lang != tolang:
--- loadlang.py
+++ loadlang.py
@@ -1,4 +1,4 @@
-import re
+import re, itertools
 class Morpheme:
     morphemelist = {}
     def __init__(self, lang, root, pos, translations, irregularforms, props):
@@ -9,10 +9,9 @@ class Morpheme:
         self.irregularforms = irregularforms
         self.props = props
         if lang not in Morpheme.morphemelist:
-            Morpheme.morphemelist[lang] = {}
-        if pos not in Morpheme.morphemelist[lang]:
-            Morpheme.morphemelist[lang][pos] = {}
-        Morpheme.morphemelist[lang][pos][root] = self
+            Morpheme.morphemelist[lang] = {'%s=%s' % (pos, root): self}
+        else:
+            Morpheme.morphemelist[lang]['%s=%s' % (pos, root)] = self
     def gettranslations(self, langid):
         return [x for x in self.translations if x.lang == langid]
     def conjugate(self):
@@ -21,7 +20,15 @@ class Morpheme:
     def find(lang, rootstr):
         if lang not in Morpheme.morphemelist:
             parselexiconfile(lang)
-        return Morpheme.morphemelist[lang][rootstr.split('=')[0]][rootstr.split('=')[1]]
+        return Morpheme.morphemelist[lang][rootstr]
+    def findpos(lang, var):
+        assert(isinstance(var, Variable))
+        s = var.strip()
+        if s[-1] in '*?':
+            s = s[:-1]
+        for w, m in Morpheme.morphemelist[lang].items():
+            if w.split('=')[0] == s and var.checkcond(m):
+                yield m
     def __str__(self):
         #return "Morpheme(%s=%s, properties:%s, translations:%s)" % (self.pos, self.root, self.props, self.translations)
         return "Morpheme(%s=%s)" % (self.pos, self.root)
@@ -61,6 +68,35 @@ class SyntaxNode:
         return ' '.join([x.display() for x in self.children if x])
     def swapchildren(self, newch):
         return SyntaxNode(self.lang, self.nodetype, newch, self.translations)
+    def extractvariables(self, pat, callback=(lambda x: x)):
+        names = {}
+        for n, p in zip(self.children, pat.children):
+            if isinstance(p, SyntaxNode):
+                for k, v in n.extractvariables(p, callback).items():
+                    names[k] = v
+            elif p == None or p == '@':
+                pass
+            else:
+                r = callback(n)
+                names[p.name] = [r] if not isinstance(r, list) else r
+                #TODO: This could result in translating the tree many times
+                #maybe try caching it somehow?
+        return names
+    def insertvariables(self, names):
+        ret = []
+        for ch in self.children:
+            if ch == None:
+                ret.append([None])
+            elif isinstance(ch, SyntaxNode):
+                ret.append(ch.insertvariables(names))
+            elif isinstance(ch, Morpheme):
+                ret.append([ch])
+            else:
+                r = names[ch.name]
+                ret.append(r if isinstance(r, list) else [r])
+        return [self.swapchildren(rl) for rl in itertools.product(*ret)]
+    def applytranslation(self, trans, callback=(lambda x: x)):
+        return trans.result.insertvariables(self.extractvariables(trans.form, callback))
     def fromstring(lang, fstr):
         nodetype = fstr[1:].split(' ', 1)[0]
         children = []
@@ -92,9 +128,51 @@ class SyntaxNode:
                 nodes.append(SyntaxNode.fromstring(lang, ch))
             elif ch[0] != '$' and '=' in ch:
                 nodes.append(Morpheme.find(lang, ch))
+            elif ch == '@':
+                nodes.append(ch)
             else:
-                nodes.append(ch) #Not a SyntaxNode
+                nodes.append(Variable.fromstring(ch)) #Not a SyntaxNode
         return SyntaxNode(lang, nodetype, nodes, [])
+class Variable:
+    regex = re.compile('^(\\$[A-Za-z0-9\\-]+):(#?[A-Za-z0-9\\-]+[*?]?)\\((([A-Za-z0-9\\-]+)=([A-Za-z0-9\\-]+))\\)$')
+    def __init__(self, name, nodetype, prop, val):
+        self.name = name
+        self.nodetype = nodetype
+        self.prop = prop
+        self.val = val
+    def fromstring(fstr):
+        if fstr[0] == '$':
+            if ':' not in fstr and '(' not in fstr:
+                return Variable(fstr, '', '', '')
+            elif ':' not in fstr:
+                m = re.match('^(\\$[A-Za-z0-9\\-]+)\\(([A-Za-z0-9\\-]+)=([A-Za-z0-9\\-]+)\\)$', fstr)
+                return Variable(m.group(1), '', m.group(2), m.group(3))
+            elif '(' not in fstr:
+                s = fstr.split(':')
+                return Variable(s[0], s[1], '', '')
+            else:
+                m = re.match('^(\\$[A-Za-z0-9\\-]+):(#?[A-Za-z0-9\\-]+[*?]?)\\(([A-Za-z0-9\\-]+)=([A-Za-z0-9\\-]+)\\)$', fstr)
+                return Variable(m.group(1), m.group(2), m.group(3), m.group(4))
+        elif '(' not in fstr:
+            return Variable('', fstr, '', '')
+        else:
+            m = re.match('^(#?[A-Za-z0-9\\-]+[*?]?)\\((([A-Za-z0-9\\-]+)=([A-Za-z0-9\\-]+))\\)$', fstr)
+            return Variable('', m.group(1), m.group(2), m.group(3))
+    def checkcond(self, morph):
+        if morph == None:
+            return False
+        if not self.prop:
+            return True
+        if self.prop == '':
+            return True
+        return morph.props[self.prop] == self.val
+    def findandcheck(self, names):
+        return self.checkcond(names[self.name])
+    def __repr__(self):
+        return '%s:%s(%s=%s)' % (self.name, self.nodetype, self.prop, self.val)
+    def strip(self):
+        m = re.match('^#?([A-Za-z0-9\\-]+)[*?]?$', self.nodetype)
+        return m.group(1) if m else ''
 class MorphologyNode:
     def __init__(self, pos, stem, affix, mode):
         self.pos = pos
@@ -131,9 +209,12 @@ class SyntaxOutline:
 class SyntaxNodeSwap:
     def __init__(self, lang, conds, ops, variables):
         self.lang = int(lang)
+        assert(not isinstance(conds[0][0], str))
         self.conds = conds
         self.ops = ops
         self.variables = variables
+    def __repr__(self):
+        return 'SNS(%s)' % self.conds
 
 class parselines:
     withargsval = re.compile('^([A-Za-z0-9\\-]+) \\((.*?)\\): (.*)$')
@@ -196,6 +277,11 @@ class parselines:
             if c.label == key:
                 r.append(c)
         return r
+def parsething(l, s):
+    if s[0] == '[':
+        return SyntaxNode.fromstring(l, s)
+    else:
+        return Variable.fromstring(s)
 def parselexiconfile(langid):
     wordlist = parselines.fromfile('langs/%s/lexicon.txt' % langid)
     ret = []
@@ -226,14 +312,14 @@ def parselangfile(langid):
             for tr in nt['translation']:
                 node.translations.append(Translation(tr.args[0], node, SyntaxNode.fromstring(tr.args[0], tr.value)))
         else:
-            vrs = [x.value for x in nt['variable']]
+            vrs = [parsething(langid, x.value) for x in nt['variable']]
             conds = []
             ops = []
             for case in nt['option'][0]['case']:
-                nd = SyntaxNode.fromstring(langid, case['structure'][0].value)
+                nd = parsething(langid, case['structure'][0].value)
                 for tr in case['translation']:
-                    nd.translations.append(Translation(tr.args[0], nd, SyntaxNode.fromstring(tr.args[0], tr.value)))
-                conds.append(case.args)
+                    nd.translations.append(Translation(tr.args[0], nd, parsething(tr.args[0], tr.value)))
+                conds.append([parsething(langid, x) for x in case.args])
                 ops.append(nd)
             node = SyntaxNodeSwap(langid, conds, ops, vrs)
         nodetypes[nt.label] = node
