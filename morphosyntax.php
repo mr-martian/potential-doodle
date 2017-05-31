511edd10bf65a11f5459ab023bc2d271d9037f19
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2017-05-31 18:38:00 -0500

    begin morphosyntax

--- morphosyntax.php
+++ morphosyntax.php
@@ -13,6 +13,7 @@
     ?>
     <style type="text/css">
       #submit { display: none; }
+      #cats > div { display: inline-block; }
     </style>
   </head>
   <body>
@@ -26,9 +27,43 @@
       <option value="ipa">International Phonetic Alphabet (IPA)</option>
       <option value="cxs">Conlang X-Sampa (CXS)</option>
     </select>
+    <h2>Categories Marked Morphologically</h2>
+    <div id="cats"></div>
+    <h2>Inflectional Forms</h2>
+    <div id="forms"></div>
+    <h2>Word Orders</h2>
+    <div id="order"></h2>
     <button onclick="validate_all();">Save</button>
     <p id="errors"></p>
     <script>
+      var defaultcats = {
+        noun: [
+          ['plurality',
+            'singular', 'plural', 'pacual', 'singulative'],
+          ['case',
+            'nominative', 'accusative', 'dative', 'genitive', 'ergative', 'absolutive', 'vocative', 'instrumental'],
+          ['class/gender',
+            'male', 'female', 'neuter', 'animate', 'inanimate', 'fire', 'earth', 'water', 'air'],
+        ],
+        verb: [
+        ],
+        adjective: [
+        ],
+        adverb: [
+        ],
+      ];
+      var makecat = function(nam, vals, checked, selected) {
+        var el = document.createElement('div');
+        mkchk(el, nam, nam, function() {}, checked ? [nam] : []);
+        var ls = document.createElement('div');
+        ls.style['margin-left'] = '15px';
+        for (var i = 0; i < vals.length; i++) {
+          mkchk(ls, vals[i], null, function() {}, selected);
+          ls.appendChild(mkel('br', ''));
+        }
+        el.appendChild(ls);
+        getel('cats').appendChild(el);
+      };
       var validate_all = function() {
         var pass = {allophony: [], phonotactics: []};
         getel('id').value = langdata.id;
